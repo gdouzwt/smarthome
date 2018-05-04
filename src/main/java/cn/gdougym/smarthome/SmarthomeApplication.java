d8f371dea8336f5d1cefdc571661196a9aef7473
zhaowentao <gdouzwt@gmail.com>
2018-05-04 11:27:33 +0800

    换回spring boot 2.0.1一样可以ajp

--- src/main/java/cn/gdougym/smarthome/SmarthomeApplication.java
+++ src/main/java/cn/gdougym/smarthome/SmarthomeApplication.java
@@ -3,8 +3,7 @@ package cn.gdougym.smarthome;
 import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -15,18 +14,20 @@ public class SmarthomeApplication {
     }
 
     @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
+    public TomcatServletWebServerFactory servletContainer() {
 
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
 
         Connector ajpConnector = new Connector("AJP/1.3");
-        ajpConnector.setProtocol("AJP/1.3");
+//        ajpConnector.setProtocol("AJP/1.3");
         ajpConnector.setPort(9090);
         ajpConnector.setSecure(false);
         ajpConnector.setAllowTrace(false);
         ajpConnector.setScheme("http");
         tomcat.addAdditionalTomcatConnectors(ajpConnector);
-
         return tomcat;
+
     }
+
+
 }
