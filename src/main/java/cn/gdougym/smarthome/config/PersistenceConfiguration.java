cf9c4f4acaf1090dfb439b19bf2861c6994ad749
zhaowentao <gdouzwt@gmail.com>
2018-05-04 10:02:35 +0800

    服务器换成apache2，使用ajp连接，退回spring boot 1.5.9

--- src/main/java/cn/gdougym/smarthome/config/PersistenceConfiguration.java
+++ src/main/java/cn/gdougym/smarthome/config/PersistenceConfiguration.java
@@ -1,7 +1,7 @@
 package cn.gdougym.smarthome.config;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
